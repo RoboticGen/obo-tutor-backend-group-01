a9ad9b74e9e7ecc03146e1589b93d6c0b0e07732
senaMora <senarathnalpsuk.21@uom.lk>
2024-10-12 15:43:54 +0530

    signup test

--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,70 @@
 from fastapi.testclient import TestClient
-from main import app
+from sqlalchemy import StaticPool, create_engine
+from sqlalchemy.orm import sessionmaker 
+from main import app, get_db
+from database import Base
+
+TEST_DATABASE_URL = "sqlite:///:memory:"
+
+engine  =  create_engine(
+    TEST_DATABASE_URL,
+    connect_args={
+        "check_same_thread": False,
+    },
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 
 client = TestClient(app)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+    
+app.dependency_overrides[get_db] = override_get_db
+
+
 
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Server is running"
+    
+def test_signup():
+    setup()
+    input_data = {
+        "first_name": "Pasindu",
+        "last_name": "Sankalpa",
+        "email": "abc@gmail.com",
+        "password": "123abcABC",
+        "phone_number": "0702225222",
+        "role": "Student",
+        "age": 20,
+        "communication_rating": 5,
+        "leadership_rating": 9,
+        "behaviour_rating": 7,
+        "responsiveness_rating": 4,
+        "difficult_concepts": "string",
+        "understood_concepts": "string",
+        "activity_summary": "string",
+        "tone_style": "string"
+    }
+    response = client.post("/api/signup", json=input_data)
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["user_id"] == 1
+    
+    teardown()
+
+
+def setup():
+    Base.metadata.create_all(bind=engine)
+    
+def teardown():
+    Base.metadata.drop_all(bind=engine)
     
